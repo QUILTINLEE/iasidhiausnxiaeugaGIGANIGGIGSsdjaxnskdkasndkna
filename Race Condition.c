e5adfb9205b0ccf725a60e92642848d1b0a0c1af
QUILTINLEE <justinquillano4@gmail.com>
2024-09-22 21:31:04 +0800

    Add files via upload

---
+++ Race Condition.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int count = 0;
+
+void *increment(void *arg) {
+    for (int i = 0; i < 100000; i++) {
+        count++;
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread1, thread2;
+
+    pthread_create(&thread1, NULL, increment, NULL);
+    pthread_create(&thread2, NULL, increment, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Final count: %d\n", count);
+
+    return 0;
+}
